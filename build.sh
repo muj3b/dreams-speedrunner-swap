78bb73655be44046230b9c065d95693714837c63
Mujeeb <mujeb@Mujeebs-MacBook-Pro.local>
2025-09-03 14:15:10 -0500

    build: correct build script output to version 2.1

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ echo "Building SpeedrunnerSwap plugin..."
 mvn clean package
 echo ""
 echo "If build was successful, the plugin jar can be found at:"
-echo "target/speedrunnerswap-2.3.0.jar"
+echo "target/speedrunnerswap-2.1.jar"
 echo ""
